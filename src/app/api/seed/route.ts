2304ff645abc67c5de147e2534fe033f29c7cdfa
BrunoAlan <alan.brunoab@gmail.com>
2024-11-22 20:36:52 -0300

    Refactor GET seed route to remove unused parameters and clean up code

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,7 +1,6 @@
 import prisma from '@/lib/prisma';
-import { NextResponse, NextRequest } from 'next/server';
 
-export async function GET(request: Request) {
+export async function GET() {
 
     await prisma.todo.deleteMany();
 
