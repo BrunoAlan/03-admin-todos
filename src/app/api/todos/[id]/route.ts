e0583e5517ad82eddbcb433488ca8f220c82427e
BrunoAlan <alan.brunoab@gmail.com>
2024-11-23 11:51:36 -0300

    Change GET API route to use findFirst for fetching a todo by ID

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,5 @@
 import prisma from '@/lib/prisma';
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 interface Segments {
     params: {
@@ -10,7 +10,7 @@ interface Segments {
 export async function GET(request: Request, { params }: Segments) {
     const { id } = params;
 
-    const todo = await prisma.todo.findUnique({
+    const todo = await prisma.todo.findFirst({
             where: {
                   id: (id),
             },
