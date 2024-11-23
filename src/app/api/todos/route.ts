f8f52bf7ffb1b8c99eeab9cb4ba9f860f95aeb1d
BrunoAlan <alan.brunoab@gmail.com>
2024-11-23 12:25:26 -0300

    Add POST API route for creating a new todo

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,24 +1,36 @@
 import prisma from '@/lib/prisma';
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-
-    const {searchParams}= new URL(request.url);
-    const take = Number(searchParams.get('take') ?? '10')
-    const skip = Number(searchParams.get('skip') ?? '0')
+    const { searchParams } = new URL(request.url);
+    const take = Number(searchParams.get('take') ?? '10');
+    const skip = Number(searchParams.get('skip') ?? '0');
 
     if (isNaN(+take)) {
-      return NextResponse.json({ error: 'Invalid take parameter' }, { status: 400 });
+        return NextResponse.json(
+            { error: 'Invalid take parameter' },
+            { status: 400 }
+        );
     }
     if (isNaN(+skip)) {
-      return NextResponse.json({ error: 'Invalid skip parameter' }, { status: 400 });
+        return NextResponse.json(
+            { error: 'Invalid skip parameter' },
+            { status: 400 }
+        );
     }
 
-      
     const todos = await prisma.todo.findMany({
-      take,
-      skip
+        take,
+        skip,
     });
 
-  return NextResponse.json(todos);
+    return NextResponse.json(todos);
+}
+
+export async function POST(request: Request) {
+    const body = await request.json();
+
+    const todo = await prisma.todo.create({ data: body });
+
+    return NextResponse.json(todo);
 }
