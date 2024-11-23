d6c95b691f5e94f85b42144fd9919e7227a2c372
BrunoAlan <alan.brunoab@gmail.com>
2024-11-23 14:55:54 -0300

    Add validation to PUT API route for updating a todo

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,14 +1,15 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
+import * as yup from 'yup';
 
 interface Segments {
-    params: {
+    params: Promise<{
         id: string;
-    };
+    }>;
 }
 
 export async function GET(request: Request, { params }: Segments) {
-    const { id } = params;
+    const { id } = await params;
 
     const todo = await prisma.todo.findFirst({
         where: {
@@ -23,8 +24,13 @@ export async function GET(request: Request, { params }: Segments) {
     return NextResponse.json(todo);
 }
 
+const putScheme = yup.object({
+    description: yup.string().optional(),
+    complete: yup.boolean().optional(),
+});
+
 export async function PUT(request: Request, { params }: Segments) {
-    const { id } = params;
+    const { id } = await params;
 
     const todo = await prisma.todo.findFirst({
         where: {
@@ -36,16 +42,25 @@ export async function PUT(request: Request, { params }: Segments) {
         return NextResponse.json({ error: 'Not found' }, { status: 404 });
     }
 
-    const body = await request.json();
+    try {
+        const { description, complete } = await putScheme.validate(
+            await request.json()
+        );
 
-    const updatedTodo = await prisma.todo.update({
-        where: {
-            id: id,
-        },
-        data: {
-            ...body,
-        },
-    });
-
-    return NextResponse.json(updatedTodo);
+        const updatedTodo = await prisma.todo.update({
+            where: {
+                id: id,
+            },
+            data: {
+                description,
+                complete,
+            },
+        });
+        return NextResponse.json(updatedTodo);
+    } catch {
+        return NextResponse.json(
+            { error: 'Validation error' },
+            { status: 400 }
+        );
+    }
 }
