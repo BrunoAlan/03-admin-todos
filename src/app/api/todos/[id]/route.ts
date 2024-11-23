bb4dc6bd998a11e136a46b99f13d159aba59a9d0
BrunoAlan <alan.brunoab@gmail.com>
2024-11-23 12:54:49 -0300

    Add PUT API route for updating a todo by ID

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -11,15 +11,41 @@ export async function GET(request: Request, { params }: Segments) {
     const { id } = params;
 
     const todo = await prisma.todo.findFirst({
-            where: {
-                  id: (id),
-            },
-      });
+        where: {
+            id: id,
+        },
+    });
 
-      if (!todo) {
-            return NextResponse.json({ error: 'Not found' }, { status: 404 });
-      }
-
+    if (!todo) {
+        return NextResponse.json({ error: 'Not found' }, { status: 404 });
+    }
 
     return NextResponse.json(todo);
+}
+
+export async function PUT(request: Request, { params }: Segments) {
+    const { id } = params;
+
+    const todo = await prisma.todo.findFirst({
+        where: {
+            id: id,
+        },
+    });
+
+    if (!todo) {
+        return NextResponse.json({ error: 'Not found' }, { status: 404 });
+    }
+
+    const body = await request.json();
+
+    const updatedTodo = await prisma.todo.update({
+        where: {
+            id: id,
+        },
+        data: {
+            ...body,
+        },
+    });
+
+    return NextResponse.json(updatedTodo);
 }
