137d56e9d0467478bb86be0687d20c1c050a702e
BrunoAlan <alan.brunoab@gmail.com>
2024-11-27 20:26:34 -0300

    Refactor RestTodosPage to use TodosGrid component for displaying todos

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,21 +1,17 @@
-import prisma from "@/lib/prisma";
+import prisma from '@/lib/prisma';
+import { TodosGrid } from '@/todos';
 
 export const metadata = {
- title: 'TODO List',
- description: 'Todo Title',
+    title: 'TODO List',
+    description: 'Todo Title',
 };
 
-
 export default async function RestTodosPage() {
+    const todos = await prisma.todo.findMany({
+        orderBy: {
+            description: 'desc',
+        },
+    });
 
-  const todos = await prisma.todo.findMany({
-    orderBy:{
-      description:"desc"}
-  })
-
-  return (
-    <div>
-      <h1>{JSON.stringify(todos)}</h1>
-    </div>
-  );
+    return <TodosGrid todos={todos} />;
 }
