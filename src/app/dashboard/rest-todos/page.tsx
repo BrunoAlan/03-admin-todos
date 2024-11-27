2519b5caa031a9d5071defde53758e31466a2f0d
BrunoAlan <alan.brunoab@gmail.com>
2024-11-27 12:33:26 -0300

    Fetch and display todos from the database on the RestTodosPage

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,21 @@
+import prisma from "@/lib/prisma";
 
-export default function RestTodosPage() {
+export const metadata = {
+ title: 'TODO List',
+ description: 'Todo Title',
+};
+
+
+export default async function RestTodosPage() {
+
+  const todos = await prisma.todo.findMany({
+    orderBy:{
+      description:"desc"}
+  })
+
   return (
     <div>
-      <h1>Hello Page</h1>
+      <h1>{JSON.stringify(todos)}</h1>
     </div>
   );
 }
