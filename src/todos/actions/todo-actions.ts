01f792210b3b0a011efd636ac7c78a51becb4718
BrunoAlan <alan.brunoab@gmail.com>
2024-12-09 21:05:32 -0300

    Add sleep function to delay toggleTodo action by 3 seconds

--- src/todos/actions/todo-actions.ts
+++ src/todos/actions/todo-actions.ts
@@ -4,10 +4,17 @@ import prisma from '@/lib/prisma';
 import { Todo } from '@prisma/client';
 import { revalidateTag } from 'next/cache';
 
+export const sleep = async (seconds: number = 0) => {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(true), seconds * 1000);
+    });
+};
+
 export const toggleTodo = async (
     id: string,
     complete: boolean
 ): Promise<Todo> => {
+    await sleep(3);
     const todo = await prisma.todo.findFirst({
         where: { id },
     });
