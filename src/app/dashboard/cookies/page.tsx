3aee42b43e630a74376f07c4471028522d6ec948
BrunoAlan <alan.brunoab@gmail.com>
2024-12-15 12:24:52 -0300

    Add CookiesPage component with metadata and basic structure

---
+++ src/app/dashboard/cookies/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Cookies Page',
+    description: 'Cookies Page',
+};
+
+export default function CookiesPage() {
+    return (
+        <div>
+            <h1>Hello Page Cookies</h1>
+        </div>
+    );
+}
