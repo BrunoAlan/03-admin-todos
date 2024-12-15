b48cbf0c7f26d859839b705e1949ab69e35776f2
BrunoAlan <alan.brunoab@gmail.com>
2024-12-15 12:43:41 -0300

    Refactor TabBar component to dynamically generate tab options and improve layout

--- src/todos/components/TabBar.tsx
+++ src/todos/components/TabBar.tsx
@@ -1,33 +1,24 @@
+const tabOptions = [1, 2, 3, 4, 5];
+
 export const TabBar = () => {
+    const gridClass = `grid-cols-${tabOptions.length}`;
+
     return (
-        <div className='grid w-full grid-cols-4 space-x-2 rounded-xl bg-gray-200 p-2'>
-            <div>
-                <input type='radio' id='1' className='peer hidden' />
-                <label className='block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white'>
-                    1
-                </label>
-            </div>
-
-            <div>
-                <input type='radio' id='2' className='peer hidden' />
-                <label className='block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white'>
-                    2
-                </label>
-            </div>
-
-            <div>
-                <input type='radio' id='3' className='peer hidden' />
-                <label className='block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white'>
-                    3
-                </label>
-            </div>
-
-            <div>
-                <input type='radio' id='4' className='peer hidden' />
-                <label className='block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white'>
-                    4
-                </label>
-            </div>
+        <div
+            className={`grid w-full space-x-2 rounded-xl bg-gray-200 p-2 ${gridClass} `}
+        >
+            {tabOptions.map((tab) => (
+                <div key={tab}>
+                    <input
+                        type='radio'
+                        id={tab.toString()}
+                        className='peer hidden'
+                    />
+                    <label className='block cursor-pointer select-none rounded-xl p-2 text-center peer-checked:bg-blue-500 peer-checked:font-bold peer-checked:text-white'>
+                        {tab}
+                    </label>
+                </div>
+            ))}
         </div>
     );
 };
