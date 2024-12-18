f6573651a02855995dcfea448cbc3976e9889a00
BrunoAlan <alan.brunoab@gmail.com>
2024-12-18 11:04:57 -0300

    Update TopMenu to fetch cart items from cookies and display item count

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,3 +1,4 @@
+import { cookies } from 'next/headers';
 import {
     CiChat1,
     CiMenuBurger,
@@ -5,7 +6,15 @@ import {
     CiShoppingBasket,
 } from 'react-icons/ci';
 
-export const TopMenu = () => {
+export const TopMenu = async () => {
+    const cookieStore = cookies();
+    const cart = JSON.parse((await cookieStore).get('cart')?.value ?? '{}');
+
+    const cartItems = Object.values<number>(cart).reduce(
+        (acc, curr) => acc + curr,
+        0
+    );
+
     return (
         <div className='sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5'>
             <div className='px-6 flex items-center justify-between space-x-4'>
@@ -41,7 +50,9 @@ export const TopMenu = () => {
                         <CiChat1 size={25} />
                     </button>
                     <button className='flex items-center justify-center p-2 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'>
-                        <span className='text-sm mr-2 font-bold'>10</span>
+                        <span className='text-sm mr-2 font-bold'>
+                            {cartItems}
+                        </span>
                         <CiShoppingBasket size={25} />
                     </button>
                 </div>
