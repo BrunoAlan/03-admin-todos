ba12e24a300666b8627ffdaf05878587cace6119
BrunoAlan <alan.brunoab@gmail.com>
2024-12-26 12:04:47 -0300

    Add summary section to CartPage displaying total price and taxes

--- src/app/dashboard/cart/page.tsx
+++ src/app/dashboard/cart/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers';
 import { ItemCard } from '@/shopping-cart';
 import { products, type Product } from '@/products/data/products';
+import { WidgetItem } from '@/components';
 
 export const metadata = {
     title: 'Shopping Cart',
@@ -27,6 +28,11 @@ export default async function CartPage() {
     const cart = JSON.parse((await cookiesStore).get('cart')?.value ?? '{}');
     const productsInCart = getProductsInCart(cart);
 
+    const total = productsInCart.reduce(
+        (acc, { product, quantity }) => acc + product.price * quantity,
+        0
+    );
+
     return (
         <div>
             <h1 className='text-5xl'>Products in cart</h1>
@@ -40,6 +46,19 @@ export default async function CartPage() {
                             quantity={quantity}
                         />
                     ))}
+                </div>
+
+                <div className='flex flex-col w-full sm:w-4/12'>
+                    <WidgetItem title='Summary'>
+                        <div className='mt-2 flex justify-center gap-4'>
+                            <h3 className='text-3xl font-bold text-gray-700'>
+                                $ {(total * 1.15).toFixed(2)}
+                            </h3>
+                        </div>
+                        <span className='font-bold text-center text-gray-500'>
+                            Taxes 15%: $ {(total * 0.15).toFixed(2)}
+                        </span>
+                    </WidgetItem>
                 </div>
             </div>
         </div>
