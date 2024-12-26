544ec08976840b526f22b86ec627fa39ab74ccca
BrunoAlan <alan.brunoab@gmail.com>
2024-12-26 11:23:33 -0300

    Replace cart button with a link to the dashboard cart for improved navigation

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,4 +1,5 @@
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 import {
     CiChat1,
     CiMenuBurger,
@@ -49,12 +50,15 @@ export const TopMenu = async () => {
                     <button className='flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'>
                         <CiChat1 size={25} />
                     </button>
-                    <button className='flex items-center justify-center p-2 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'>
+                    <Link
+                        href={'/dashboard/cart'}
+                        className='flex items-center justify-center p-2 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200'
+                    >
                         <span className='text-sm mr-2 font-bold'>
                             {cartItems}
                         </span>
                         <CiShoppingBasket size={25} />
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
