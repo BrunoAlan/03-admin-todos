27823f738f3fb54ff476150d2bd3844776713deb
BrunoAlan <alan.brunoab@gmail.com>
2025-01-06 19:53:09 -0300

    Implement server-side session check in DashboardPage and display user info

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,16 @@
-import { WidgetItem } from "@/components";
+import { WidgetItem } from '@/components';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
-export default function DashboardPage() {
-  return (
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-            
-      <WidgetItem/>
+export default async function DashboardPage() {
+    const session = await getServerSession();
+    if (!session) redirect('/api/auth/signin');
 
-    </div>  
-  );
+    return (
+        <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
+            <WidgetItem title='User logged'>
+                {JSON.stringify(session.user)}
+            </WidgetItem>
+        </div>
+    );
 }
