6b7cb1293110f503bb1c82acaaf2aa24cc99c235
BrunoAlan <alan.brunoab@gmail.com>
2025-01-06 19:56:01 -0300

    Refactor DashboardPage to display user information in a structured format

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,9 +7,13 @@ export default async function DashboardPage() {
     if (!session) redirect('/api/auth/signin');
 
     return (
-        <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
+        <div className='grid gap-6 md:grid-cols-1 lg:grid-cols-2'>
             <WidgetItem title='User logged'>
-                {JSON.stringify(session.user)}
+                <div className='flex flex-col'>
+                    <span>{session.user?.name}</span>
+                    <span>{session.user?.email}</span>
+                    <span>{session.user?.image}</span>
+                </div>
             </WidgetItem>
         </div>
     );
