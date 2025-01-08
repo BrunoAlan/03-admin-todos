bca3ae081fcf387118df4df58445f70716ab444e
BrunoAlan <alan.brunoab@gmail.com>
2025-01-08 12:37:13 -0300

    Refactor NextAuth configuration to include session callbacks for role and ID management

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,9 +6,6 @@ import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
-    session: {
-        strategy: 'jwt',
-    },
     adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GoogleProvider({
@@ -20,6 +17,32 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GITHUB_SECRET ?? '',
         }),
     ],
+    session: {
+        strategy: 'jwt',
+    },
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            return true;
+        },
+        async jwt({ token, user, account, profile }) {
+            const dbUser = await prisma.user.findUnique({
+                where: {
+                    email: token.email ?? 'no-email',
+                },
+            });
+            token.roles = dbUser?.roles ?? ['no-roles'];
+            token.id = dbUser?.id ?? 'no-id';
+            return token;
+        },
+
+        async session({ session, token, user }) {
+            if (session && session.user) {
+                session.user.roles = token.roles;
+                session.user.id = token.id;
+            }
+            return session;
+        },
+    },
 };
 
 const handler = NextAuth(authOptions);
