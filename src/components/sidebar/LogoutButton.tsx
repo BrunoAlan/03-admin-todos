f908a174c4530c3f4f4bcba086cd8fd05036e018
BrunoAlan <alan.brunoab@gmail.com>
2025-01-08 11:07:45 -0300

    Enhance LogoutButton component to handle loading and authentication states

--- src/components/sidebar/LogoutButton.tsx
+++ src/components/sidebar/LogoutButton.tsx
@@ -1,10 +1,35 @@
 'use client';
 
-import { CiLogout } from 'react-icons/ci';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { CiLogout, CiCloudRainbow, CiLogin } from 'react-icons/ci';
 
 export const LogoutButton = () => {
+    const { data: session, status } = useSession();
+
+    if (status === 'loading')
+        return (
+            <button className='px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group'>
+                <CiCloudRainbow />
+                <span className='group-hover:text-gray-700'>Logout</span>
+            </button>
+        );
+
+    if (status === 'authenticated')
+        return (
+            <button
+                onClick={() => signOut()}
+                className='px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group'
+            >
+                <CiLogin />
+                <span className='group-hover:text-gray-700'>Logout</span>
+            </button>
+        );
+
     return (
-        <button className='px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group'>
+        <button
+            onClick={() => signOut()}
+            className='px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group'
+        >
             <CiLogout />
             <span className='group-hover:text-gray-700'>Logout</span>
         </button>
