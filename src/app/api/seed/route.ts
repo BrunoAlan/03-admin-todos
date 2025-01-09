4b50cf7354999248b227d074205130bce30a18db
BrunoAlan <alan.brunoab@gmail.com>
2025-01-09 11:57:38 -0300

    Seed database with a test user and associated todos, including password hashing

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,24 +1,29 @@
 import prisma from '@/lib/prisma';
+import bcrypt from 'bcryptjs';
 
 export async function GET() {
-
     await prisma.todo.deleteMany();
-
-    await prisma.todo.createMany({
-        data:[
-            {description: 'Buy milk',complete: true},
-            {description: 'Buy bread'},
-            {description: 'Buy eggs'},
-            {description: 'Buy butter'},
-            {description: 'Buy sugar'},
-        ]
+    await prisma.user.deleteMany();
+    const user = await prisma.user.create({
+        data: {
+            email: 'test1@google.com',
+            password: bcrypt.hashSync('123456', 10),
+            roles: ['admin,client,super-user'],
+            todos: {
+                create: [
+                    { description: 'Buy milk', complete: true },
+                    { description: 'Buy bread' },
+                    { description: 'Buy eggs' },
+                    { description: 'Buy butter' },
+                    { description: 'Buy sugar' },
+                ],
+            },
+        },
     });
-
-
     return new Response(
         JSON.stringify({
             message: 'Seed executed successfully',
-            
+            user,
         })
     );
 }
